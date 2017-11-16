773112eac138af1dff12d827390e666f58a4dbd0
lluis <lluis@ngynx.com>
2017-11-16 20:57:10 +0100

    enhanced rd

---
+++ src/RALLF/Integration/RallfLogger.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluis
+ * Date: 11/16/17
+ * Time: 7:42 PM
+ */
+
+namespace RALLF\Integration;
+
+class RallfLogger extends AbstractLogger {
+
+    /**
+     * @param string $message
+     * @param mixed $data
+     * @param int $severity , standard RFC3164 code (https://tools.ietf.org/html/rfc3164)
+     * @param string $channel
+     */
+    function log($message, $data = null, $severity = Logger::SEVERITY_DEBUG, $channel = "")
+    {
+        echo $channel . "[" . $severity . "]: $message";
+        if($data != null) echo " (" . json_encode($data) . ")";
+        echo "\n";
+    }
+}
